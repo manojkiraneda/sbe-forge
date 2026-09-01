de4d41308ca448c2bf87ef8cbac6432a2dc5a890
Manojkiran Eda <manojkiran.eda@gmail.com>
2026-09-02 00:29:02 +0530

    firmware: signal completion from hello apps

--- firmware/apps/hello/main.c
+++ firmware/apps/hello/main.c
@@ -1,8 +1,11 @@
 #include "sbe/printf.h"
 
+#define TEST_REGISTER ((volatile unsigned long long *)0x50009u)
+
 int main(void)
 {
     printf("Hello world");
+    *TEST_REGISTER |= 1ULL;
     while (1)
     {
         // Infinite loop
